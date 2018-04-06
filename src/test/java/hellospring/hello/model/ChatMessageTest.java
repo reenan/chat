fa59536fd12e5530158977fc209e2e23d9d78793
Renan <0renan.souza@gmail.com>
2018-04-06 16:12:39 -0300

    Initial commit

---
+++ src/test/java/hellospring/hello/model/ChatMessageTest.java
@@ -0,0 +1,12 @@
+package hellospring.hello.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ChatMessageTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
